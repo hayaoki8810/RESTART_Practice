360b97d14a3f84fe85ef0587c8c984c1d822dc70
Hayato Aoki <81552478+Hayato-Aoki@users.noreply.github.com>
2022-03-22 20:16:53 +0900

    Initial Commit

---
+++ RESTART_Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RESTART_Practice
+//
+//  Created by Hayato Aoki on 3/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RESTART_Practice/RESTART_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTART_PracticeApp.swift
+//  RESTART_Practice
+//
+//  Created by Hayato Aoki on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RESTART_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
